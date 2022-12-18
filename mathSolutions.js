734222be43bdec163e32877cb202bd02c3fa161e
Atif Maqsood <atifmaqsood907@gmail.com>
2022-12-18 15:35:00 +0500

    Make a Function in js to check whether a given string is palindrome or not

---
+++ mathSolutions.js
@@ -0,0 +1,28 @@
+// function to check if the string is palindrome or not
+
+function checkPalindrome(userInput) {
+    
+    // converting string into array
+    const arrayValues = userInput.split('');
+   
+    // reversing the array value
+    const reverseArrayValue = arrayValues.reverse();
+
+    // converting array into string
+    const reverseString = reverseArrayValue.join('');
+
+    // check whether a string is palindrome or not
+    if (userInput == reverseString) {
+        console.log("It is a palindrome");
+    }
+    else{
+        console.log("It is not a palindrome");
+    }
+
+    // take input
+    const userInput = prompt('Enter a value ');
+
+    checkPalindrome(userInput);
+
+
+}
